e585a9f45d6fc05deda54f0717fd065ca597a9b6
James Esguerra <jamesesguerra025@gmail.com>
2024-09-20 20:21:21 +0800

    make recipe detail page responsive

--- src/app/pages/recipes/recipe-detail/recipe-content/recipe-content.component.ts
+++ src/app/pages/recipes/recipe-detail/recipe-content/recipe-content.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-content',
   templateUrl: './recipe-content.component.html',
-  styleUrl: './recipe-content.component.scss'
+  styleUrl: './recipe-content.component.scss',
+  host: {
+    '(resize)': 'onResize()'
+  }
 })
-export class RecipeContentComponent {
+export class RecipeContentComponent implements OnInit, OnDestroy {
+  resizeSubscription!: Subscription;
+  screenSize: 'sm' | 'md' = window.innerWidth < 645 ? 'sm' : 'md';
   rating = 5;
   value = 0;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.resizeSubscription = fromEvent(window, 'resize')
+    .subscribe((event: Event) => {
+      const width = window.innerWidth;
+
+      this.screenSize = width < 645 ? 'sm' : 'md';
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.resizeSubscription) {
+      this.resizeSubscription.unsubscribe();
+    }
+  }
+
 }
