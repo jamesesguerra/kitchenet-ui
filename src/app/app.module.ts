c9746931b52963f118c7ab66c74d26b20308119e
James Esguerra <jamesesguerra025@gmail.com>
2024-11-09 10:24:36 +0800

    delete extra files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,6 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
-import { ProductService } from './demo/service/product.service';
-import { CountryService } from './demo/service/country.service';
-import { CustomerService } from './demo/service/customer.service';
-import { EventService } from './demo/service/event.service';
-import { IconService } from './demo/service/icon.service';
-import { NodeService } from './demo/service/node.service';
-import { PhotoService } from './demo/service/photo.service';
 import { AuthModule } from '@auth0/auth0-angular';
 import { environment as env } from 'src/environments/environment';
 
@@ -25,8 +18,6 @@ import { environment as env } from 'src/environments/environment';
     ],
     providers: [
         { provide: LocationStrategy, useClass: PathLocationStrategy },
-        CountryService, CustomerService, EventService, IconService, NodeService,
-        PhotoService, ProductService
     ],
     bootstrap: [AppComponent],
 })
