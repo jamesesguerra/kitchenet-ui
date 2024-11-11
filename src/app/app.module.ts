4da535b50f046543e5591d855f90c048a69d4ee6
James Esguerra <jamesesguerra025@gmail.com>
2024-11-11 09:16:48 +0800

    edit not found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 import { AuthModule } from '@auth0/auth0-angular';
 import { environment as env } from 'src/environments/environment';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
     declarations: [AppComponent, NotfoundComponent],
@@ -14,7 +15,8 @@ import { environment as env } from 'src/environments/environment';
         AppLayoutModule,
         AuthModule.forRoot({
             ...env.auth0
-        })
+        }),
+        ButtonModule
     ],
     providers: [
         { provide: LocationStrategy, useClass: PathLocationStrategy },
